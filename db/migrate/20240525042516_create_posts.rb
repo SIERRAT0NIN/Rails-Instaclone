8d0261062c08d48aa9793c28470109df7ea84021
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-05-24 21:27:35 -0700

    inital push, ruby-on-rails

---
+++ db/migrate/20240525042516_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :string
+      t.string :description
+      t.string :keywords
+
+      t.timestamps
+    end
+  end
+end
