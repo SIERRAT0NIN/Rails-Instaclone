dd40acaf340929271a0df4e8c95bb23b2ebd537d
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-05-24 21:51:51 -0700

    schaffold, db migrate

--- db/migrate/20240525042516_create_posts.rb
+++ db/migrate/20240525042516_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :string
       t.string :description
       t.string :keywords
 
